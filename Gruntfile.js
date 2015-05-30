d2fa7b4cea7754beb7ddfb97c9ef77850b601ce2
Dennis Frank <mail@dennisfrank.info>
2015-05-30 16:53:08 +0200

    Use Grunt template strings for path definitions

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,28 +7,33 @@ module.exports = function(grunt) {
         // Reference the source paths.
         source: {
             source: 'source',
-            css: 'source/css',
-            stylesheet: 'source/css/style.scss',
-            js: 'source/js',
-            fonts: 'source/fonts',
-            img: 'source/images'
+            assets: '<%= globalConfig.source.source %>/assets',
+            css: '<%= globalConfig.source.assets %>/scss',
+            stylesheet: '<%= globalConfig.source.css %>/style.scss',
+            js: '<%= globalConfig.source.assets %>/js',
+            fonts: '<%= globalConfig.source.assets %>/fonts',
+            img: '<%= globalConfig.source.assets %>/images'
         },
         // The Pattern Lab destination directory.
         public: {
             public: 'public',
-            css: 'public/css',
-            stylesheet: 'public/css/style.css',
-            js: 'public/js',
-            fonts: 'public/fonts',
-            img: 'public/images'
+            assets: '<%= globalConfig.public.public %>/assets',
+            css: '<%= globalConfig.public.assets %>/css',
+            stylesheet: '<%= globalConfig.public.css %>/style.css',
+            js: '<%= globalConfig.public.assets %>/js',
+            fonts: '<%= globalConfig.public.assets %>/fonts',
+            img: '<%= globalConfig.public.assets %>/images'
         },
 
         // Adjust these values to the assets destination paths of your cms
         cms: {
-            css: 'cms/css',
-            stylesheet: 'cms/css/style.css',
-            js: 'cms/js',
-            fonts: 'cms/fonts'
+            cms: 'cms',
+            assets: '<%= globalConfig.cms.cms %>/assets',
+            css: '<%= globalConfig.cms.assets %>/css',
+            stylesheet: '<%= globalConfig.cms.css %>/style.css',
+            js: '<%= globalConfig.cms.assets %>/js',
+            fonts: '<%= globalConfig.cms.assets %>/fonts',
+            img: '<%= globalConfig.cms.assets %>/images'
         }
 
     };

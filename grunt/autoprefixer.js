d50138ad448aafac558cefc55c5d43768d40c549
Dennis Frank <mail@dennisfrank.info>
2015-08-10 01:04:29 +0200

    Update autoprefixer task

--- grunt/autoprefixer.js
+++ grunt/autoprefixer.js
@@ -3,13 +3,22 @@ module.exports = {
         browsers: [
             'last 3 version',
             'ie 9',
-            'ie 10'
+            'ie 10',
+            'ie 11'
         ]
     },
     dev: {
-        src: '<%= globalConfig.public.stylesheet %>'
+        expand: true,
+        flatten: true,
+        cwd: '<%= globalConfig.public.css %>/',
+        src: '**/*.css',
+        dest: '<%= globalConfig.public.css %>/'
     },
     cms: {
-        src: '<%= globalConfig.cms.stylesheet %>'
+        expand: true,
+        flatten: true,
+        cwd: '<%= globalConfig.cms.css %>/',
+        src: '**/*.css',
+        dest: '<%= globalConfig.cms.css %>/'
     }
 };
